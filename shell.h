f6d9cbfb14822ed0dbc90d7493fdb21624c93c82
DJPooja <poojadurgoji@gmail.com>
2018-05-31 18:57:06 +0530

    Add files via upload

---
+++ shell.h
@@ -0,0 +1,51 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<fcntl.h>
+#include<time.h>
+#include<stdint.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+#define MAX_NO_OF_COMMANDS 25
+#define NO_OF_TOKENS 10
+#define COMMAND_LENGTH 40
+#define MAX_ALIASES MAX_NO_OF_COMMANDS
+#define OPTION_LENGTH COMMAND_LENGTH/2
+#define PATH_LENGTH 40
+#define MAX_FILE_SIZE 256
+#define MAX_FILENAME_LENGTH 25
+#define TIMESTAMP_LENGTH 25
+
+#define EOFile '\0'
+#define EOLine '\n'
+#define PATH_SEPARATOR '/'
+
+void pipeHandler(char * args[]);
+void fileIO(char * args[], char* inputFile, char* outputFile, int option);
+void launchProg(char **args, int background);
+int commandHandler(char * args[]);
+
+extern char** environ;
+char* currentDirectory;
+char** split(char buf[],int length);
+char** aliasNames;
+char** aliasDecoded;
+pid_t pid;
+
+struct history
+{
+	char* command;
+	pid_t commandPid;
+	char* time;
+};
+
+struct history historyTable[MAX_NO_OF_COMMANDS];
+
+int noOfCommands;
+
+char** env;
